b5f1fce88d3fd5d8e7a99bf4c87215dc1e87f34d
Nicola <91728732+NicolaPhiri@users.noreply.github.com>
2026-09-13 22:09:14 +0200

    Add files via upload

---
+++ BackAtIt1.py
@@ -0,0 +1,14 @@
+"""
+Name: Nicola
+Surname: Phiri
+Date: 13/09/26
+Program name: Age year prediction
+"""
+
+strName = str(input("What is your name?: "))
+intAge = int(input("How old are you?: "))
+intPrediction = 2026 - intAge + 30
+
+print("Hi",strName," You will turn 30 in", intPrediction,".")
+# else print(f"Hi{strName}! You will turn 30 in{intPrediction}."
+#Prevents unnecessary spacing in print message
